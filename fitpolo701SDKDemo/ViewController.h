4d2f58671c38d0c82014f03893ec0b488cf22cda
Chengang <chengang@mokotechnology.com>
2018-05-23 09:03:42 +0800

    Initial Commit

---
+++ fitpolo701SDKDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  fitpolo701SDKDemo
+//
+//  Created by aa on 2018/5/23.
+//  Copyright © 2018年 HCK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ fitpolo701SDKDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  fitpolo701SDKDemo
+//
+//  Created by aa on 2018/5/23.
+//  Copyright © 2018年 HCK. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
