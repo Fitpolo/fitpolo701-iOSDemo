e0a5a76bdd41d2538f8105649b59c3ad07528772
Chengang <chengang@mokotechnology.com>
2018-06-26 10:22:34 +0800

    first commit

--- fitpolo701SDKDemo/HCKMainViewController.h
+++ fitpolo701SDKDemo/HCKMainViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  HCKMainViewController.h
 //  fitpolo701SDKDemo
 //
 //  Created by aa on 2018/5/23.
@@ -8,8 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
-
+@interface HCKMainViewController : UIViewController
 
 @end
-
